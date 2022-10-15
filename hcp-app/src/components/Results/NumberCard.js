59e003dc69b93333f70ddab7e6b560a5bb596807
Serhan Asad <95069916+Serhan-Asad@users.noreply.github.com>
2022-10-15 15:44:56 -0400

    Update NumberCard.js

--- hcp-app/src/components/Results/NumberCard.js
+++ hcp-app/src/components/Results/NumberCard.js
@@ -1,11 +1,34 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
 
 function NumberCard() {
+    const [ecoCost,setEcoCost] = useState(getNumber(1234567829289))
+    let x = 125000;
+    const [traffic,setTraffic] = useState(x.toLocaleString())
     return ( 
-        <div>
-            <span>hi</span>
+        <div className='NumberCard'>
+            <p id='economicCost'>Total Economic cost:</p>
+            <p class='Numbers'>${ecoCost}</p>
+            <hr></hr>
+            <p>Traffic Caused:</p>
+            <p class='Numbers' id='traffic'>{traffic}</p>
+            <p id='carsBlocked'>cars blocked</p>
         </div>
      );
 }
 
+let getNumber = function(num) {
+    
+    var units = ["M","B","T","Q"]
+    var unit = Math.floor((num / 1.0e+1).toFixed(0).toString().length)
+    var r = unit%3
+    var x =  Math.abs(Number(num))/Number('1.0e+'+(unit-r)).toFixed(2)
+    if (unit<6){
+        return num
+    } else {
+        return x.toFixed(2)+ ' ' + units[Math.floor(unit / 3) - 2]
+    }
+}
+
 export default NumberCard;
+
+
